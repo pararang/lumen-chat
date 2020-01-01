ca60b848c2aa6fb2ed750ea962a961f38218d120
Muhammad Ikhsan <pararang@gmail.com>
2020-01-01 21:13:26 +0700

    add stream test

--- tests/Message/StreamMessageTest.php
+++ tests/Message/StreamMessageTest.php
@@ -4,7 +4,16 @@
 namespace Message;
 
 
-class StreamMessageTest
-{
+use App\Message;
+use Symfony\Component\HttpFoundation\Response;
+use TestCase;
 
+class StreamMessageTest extends TestCase
+{
+    protected $path = '/messages/stream';
+    public function testSuccess()
+    {
+        $this->get($this->path)
+            ->assertResponseStatus(Response::HTTP_OK);
+    }
 }
